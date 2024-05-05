fe7394a9719608707e76cd299c0ff97f24b96e9f
Redeltaz <campistron.lucas@gmail.com>
2024-05-06 01:01:38 +0200

    feat(db/logger): add db connection + format logs

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 )
@@ -12,6 +13,11 @@ type Logger interface {
 	Warn(msg string, args ...any)
 	Error(msg string, args ...any)
 	Fatal(msg string, args ...any)
+	Debugf(format string, args ...any)
+	Infof(format string, args ...any)
+	Warnf(format string, args ...any)
+	Errorf(format string, args ...any)
+	Fatalf(format string, args ...any)
 }
 
 type logger struct {
@@ -44,5 +50,31 @@ func (l *logger) Error(msg string, args ...any) {
 
 func (l *logger) Fatal(msg string, args ...any) {
 	l.yolog.Error(msg, args...)
+	os.Exit(1)
+}
+
+func (l *logger) Debugf(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.yolog.Debug(msg, args...)
+}
+
+func (l *logger) Infof(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.yolog.Info(msg, args...)
+}
+
+func (l *logger) Warnf(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.yolog.Warn(msg, args...)
+}
+
+func (l *logger) Errorf(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.yolog.Error(msg, args...)
+}
+
+func (l *logger) Fatalf(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.yolog.Error(msg)
 	os.Exit(1)
 }
