5dc4138e9901f25b730a3fe0ffb3861b0b99b937
AtoLrn <antoine.lorin2@gmail.com>
2024-07-16 17:08:14 +0200

    fix: luca's mistake

--- internal/models/project.go
+++ internal/models/project.go
@@ -2,15 +2,15 @@ package models
 
 type Project struct {
 	GormModel
-	Title      string `json:"title" gorm:"column:title"`
-	EndDate    string `json:"endDate" gorm:"column:end_date"`
-	CourseId   uint   `json:"courseId" gorm:"column:course_id"`
-	ClassId    uint   `json:"classId" gorm:"column:class_id"`
-	DocumentId uint   `json:"documentId" gorm:"column:document_id"`
-	TeacherId  uint   `json:"teacherId" gorm:"column:teacher_id"`
-	Course     Course `json:"course" gorm:"foreignKey:CourseId;references:ID"`
-	Class      Class  `json:"class" gorm:"foreignKey:ClassId;references:ID"`
-	Document   Class  `json:"document" gorm:"foreignKey:DocumentId;references:ID"`
+	Title      string   `json:"title" gorm:"column:title"`
+	EndDate    string   `json:"endDate" gorm:"column:end_date"`
+	CourseId   uint     `json:"courseId" gorm:"column:course_id"`
+	ClassId    uint     `json:"classId" gorm:"column:class_id"`
+	DocumentId uint     `json:"documentId" gorm:"column:document_id"`
+	TeacherId  uint     `json:"teacherId" gorm:"column:teacher_id"`
+	Course     Course   `json:"course" gorm:"foreignKey:CourseId;references:ID"`
+	Class      Class    `json:"class" gorm:"foreignKey:ClassId;references:ID"`
+	Document   Document `json:"document" gorm:"foreignKey:DocumentId;references:ID"`
 }
 
 type ProjectCreate struct {
